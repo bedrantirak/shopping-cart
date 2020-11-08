c46af804e4a36f16c11e6044353d92fa1d1eacdb
bedrantirak <bdrntrk@gmail.com>
2020-11-08 14:59:25 +0300

    Update App.js

--- cart/src/App.js
+++ cart/src/App.js
@@ -1,4 +1,9 @@
 import React from 'react'
+<<<<<<< Updated upstream
+=======
+import Filter from './components/Filter';
+import Products from './components/Products';
+>>>>>>> Stashed changes
 import data from "./data.json"
 
 class App extends React.Component{
@@ -11,7 +16,39 @@ class App extends React.Component{
     };
   }
 
+ sortProducts=(event)=>{
+   //imp
+   //console.log(event.target.value);
+   const sort =event.target.value;
+   this.setState((state)=> ({
+     sort:sort,
+     products:this.state.products.slice().sort((a,b)=>
+       sort === "lowest" ?
+       a.price > b.price? 1 : -1:
+       sort === "highest" ?
+       a.price < b.price? 1 : -1: 
 
+       a._id > b._id? 1 : -1
+
+
+     ),
+   }));
+ }
+
+ filterProducts =(event)=>{
+   //imp
+    //console.log(event.target.value);
+   if(event.target.value === ""){
+     this.setState({size:event.target.value, products:data.products});
+   } else{
+    this.setState({
+      size: event.target.value,
+      products: data.products.filter((product) => product.availableSizes.indexOf(event.target.value)>=0),
+ 
+    });
+   }
+  
+ }
   render(){
     return (
       <div className="grid-container">
@@ -21,7 +58,18 @@ class App extends React.Component{
         <main>
           <div className="content">
             <div className="main">
+<<<<<<< Updated upstream
            Products
+=======
+              <Filter 
+              count={this.state.products.length} 
+              size={this.state.size}
+              sort={this.state.sort}
+              filterProducts={this.filterProducts}
+              sortProducts={this.sortProducts}
+              />
+             <Products products={this.state.products}/>
+>>>>>>> Stashed changes
             </div>
             <div className="sidebar">
             Cart Items
