7dcedb929a9a88f6bf59601fa8e4c8d5fb1b2971
bedrantirak <bdrntrk@gmail.com>
2020-11-08 11:49:39 +0300

    Update App.js

--- cart/src/App.js
+++ cart/src/App.js
@@ -1,20 +1,40 @@
 import React from 'react'
+import data from "./data.json"
+
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state ={
+      products: data.products,
+      size:"",
+      sort:"",
+    };
+  }
 
 
-function App() {
-  return (
-    <div className="grid-container">
-      <header>
-        <a href="/">React Shopping Cart</a>
-      </header>
-      <main>
-        Product List
-      </main>
-      <footer>
-        All rights is reserved. | By  <a className="bedran" href="https://bedrantirak.github.io/"> Bedran</a>
-      </footer>
-    </div>
-  )
+  render(){
+    return (
+      <div className="grid-container">
+        <header>
+          <a href="/">React Shopping Cart</a>
+        </header>
+        <main>
+          <div className="content">
+            <div className="main">
+           Products
+            </div>
+            <div className="sidebar">
+            Cart Items
+            </div>
+          </div>
+        </main>
+        <footer>
+          All rights is reserved. | By  <a className="bedran" href="https://bedrantirak.github.io/"> Bedran</a>
+        </footer>
+      </div>
+    )
+  }
+  
 }
 
 export default App;
